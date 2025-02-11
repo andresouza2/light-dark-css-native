e93c1c927aa205041f0b62e9adcd7084dfd9fbca
André Costa de Souza <andre.souza@compaz.com.br>
2025-02-11 10:11:53 -0400

    chore: inserindo o theme no localstorage

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const btnToggle = document.querySelector("button[id=toggle]");
 const body = document.querySelector("body");
+const localStorage = window.localStorage;
+
+const theme = localStorage.getItem("theme");
+if (theme) {
+  body.classList.add(theme);
+}
 
 btnToggle.addEventListener("click", () => {
   if (body.classList.contains("light")) {
     body.classList.remove("light");
     body.classList.add("dark");
+    localStorage.setItem("theme", "dark");
   } else {
     body.classList.remove("dark");
     body.classList.add("light");
+    localStorage.setItem("theme", "light");
   }
 });
+
+
